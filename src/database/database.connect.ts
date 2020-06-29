b4e64b26fca3eafd8260f7c44eabd7b56e42cf56
Jordan <jordan.porras1@gmail.com>
2020-06-29 00:56:55 -0400

    Conexion a la DB

---
+++ src/database/database.connect.ts
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+export default class database {
+	private url: string;
+	private options: any = {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	};
+
+	private constructor(URL: string) {
+		this.url = URL;
+	}
+
+	static init(URL: string) {
+		return new database(URL);
+	}
+
+	async conect() {
+		mongoose
+			.connect(this.url, this.options)
+			.then(() => {
+				console.log("connected to database");
+			})
+			.catch((err) => console.log(err));
+	}
+}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import Server from "./server/server";
+import DBconnect from "./database/database.connect";
 
+const url: string = "mongodb://localhost:27017/PCollabRAE-DB";
+const database = DBconnect.init(url);
 const server = Server.init(Number(process.env.PORT || 3000));
 
+database.conect();
 server.start();
