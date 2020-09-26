f90559449d2a62c9f215220ab25ffaeceb0511d1
Jordan <jordan.porras1@gmail.com>
2020-09-26 20:29:32 -0300

    modificar querys

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -5,167 +5,152 @@ import User from '../user/user.model';
 import Comments from '../comments/comment.model';
 
 export default class PostController {
-	async savePost(req: Request, res: Response) {
-		const errors = validationResult(req);
-		if (!errors.isEmpty()) {
-			return res.status(400).send(errors);
-		}
-		let user = await User.findById(req.userId);
-		if (!user) {
-			return res.status(204).send({ message: 'Usuario no encontrado.' });
-		}
-		const { title, url, content, categorys, tags } = req.body;
-		const post = await Post.findOne({
-			$and: [{ enabled: true }, { $or: [{ url }, { title }] }]
-		});
-		if (post) {
-			return res.status(200).send({
-				message: 'Ya existe una publicación con ese titulo o url'
-			});
-		}
-		const newPost: IPost = new Post({
-			title,
-			url,
-			content,
-			categorys,
-			tags,
-			publishedBy: req.userId
-		});
-		user.posts.push(newPost._id);
-		await newPost.save();
-		await user.save();
-		return res.status(200).send(newPost);
-	}
-	async getPosts(req: Request, res: Response) {
-		const posts = await Post.find({ enabled: true })
-			.populate({
-				path: 'publishedBy',
-				model: User,
-				select: 'name'
-			})
-			.populate({
-				path: 'comments',
-				model: Comments,
-				match: { enabled: true },
-				select: 'content'
-			});
-		if (!posts) {
-			return res
-				.status(400)
-				.send({ message: 'No se encontraron publicaciones' });
-		}
-		return res.status(200).send(posts);
-	}
-	async getPost(req: Request, res: Response) {
-		const errors = validationResult(req);
-		if (!errors.isEmpty()) {
-			return res.status(400).send(errors);
-		}
-		try {
-			const post = await Post.findById(req.params.id)
-				.populate({
-					path: 'publishedBy',
-					model: User,
-					select: 'name'
-				})
-				.populate({
-					path: 'comments',
-					model: Comments,
-					match: { enabled: true },
-					select: 'content'
-				});
-			if (!post) {
-				return res
-					.status(204)
-					.send({ message: 'Publicación no encontrada.' });
-			}
-			return res.status(200).send(post);
-		} catch (error) {
-			return res.status(500).send({ message: 'Ha ocurrido un error' });
-		}
-	}
-	async modifyPost(req: Request, res: Response) {
-		const errors = validationResult(req);
-		if (!errors.isEmpty()) {
-			return res.status(400).send(errors);
-		}
-		try {
-			const { title, url, content, categorys, tags } = req.body;
-			let posturl = await Post.findById(req.params.id);
-			let post = await Post.findOne({ $or: [{ url }, { title }] });
-			if (!posturl) {
-				return res
-					.status(204)
-					.send({ message: 'Publicación no encontrada.' });
-			} else if (post) {
-				return res.status(200).send({
-					message: 'Ya existe una publicación con ese titulo o url'
-				});
-			} else if (
-				req.userId == post.publishedBy ||
-				req.userRole === 'ADMIN'
-			) {
-				post.title = title;
-				post.url = url;
-				post.content = content;
-				post.categorys = categorys;
-				post.tags = tags;
-				post.updatedAt = Date.now();
-				await post.save();
-				return res.status(200).send({ message: 'Cambios realizados.' });
-			} else {
-				return res.status(204).send({
-					message:
-						'No posees permiso para modificar esta publicación.'
-				});
-			}
-		} catch (error) {
-			return res
-				.status(500)
-				.send({ message: 'Se ha producido un error' });
-		}
-	}
-	async changePostEnabled(req: Request, res: Response) {
-		const errors = validationResult(req);
-		if (!errors.isEmpty()) {
-			return res.status(400).send(errors);
-		}
-		try {
-			let post = await Post.findById(req.params.id);
-			if (!post)
-				return res
-					.status(204)
-					.send({ message: 'Publicación no encontrada.' });
-			if (req.userId == post.publishedBy || req.userRole === 'ADMIN') {
-				post.enabled = !post.enabled;
-				await post.save();
-				//post.deleteOne(); en caso de querer borrar de verdad
-				return res.status(200).send({
-					message: 'Se cambio la visualización de la publicacion'
-				});
-			} else {
-				return res.status(200).send({
-					message: 'No posees permiso para eliminar esta publicación'
-				});
-			}
-		} catch (error) {
-			console.log(error);
-			return res.status(400).send(error);
-		}
-	}
-	async filter(req: Request, res: Response) {
-		const { title, categorys, tags } = req.body;
-		let posts = await Post.find({
-			enabled: true,
-			...(title ? { title: { $regex: title, $options: 'i' } } : {}),
-			...(categorys ? { categorys: { $all: categorys } } : {}),
-			...(tags ? { tags: { $all: tags } } : {})
-		});
-		if (!posts) {
-			return res
-				.status(400)
-				.send({ message: 'No se encontraron publicaciones' });
-		}
-		return res.status(200).send(posts);
-	}
+  async savePost(req: Request, res: Response) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).send(errors);
+    }
+    let user = await User.findById(req.userId);
+    if (!user) {
+      return res.status(204).send({ message: 'Usuario no encontrado.' });
+    }
+    const { title, url, content, categorys, tags } = req.body;
+    const post = await Post.findOne({
+      $and: [{ enabled: true }, { $or: [{ url }, { title }] }]
+    });
+    if (post) {
+      return res.status(200).send({
+        message: 'Ya existe una publicación con ese titulo o url'
+      });
+    }
+    const newPost: IPost = new Post({
+      title,
+      url,
+      content,
+      categorys,
+      tags,
+      publishedBy: req.userId
+    });
+    user.posts.push(newPost._id);
+    await newPost.save();
+    await user.save();
+    return res.status(200).send(newPost);
+  }
+  async getPosts(req: Request, res: Response) {
+    const posts = await Post.find({ enabled: true })
+      .select({ title: 1, content: 1, categorys: 1 })
+      .sort({ createdAt: -1 });
+    if (!posts) {
+      return res
+        .status(400)
+        .send({ message: 'No se encontraron publicaciones' });
+    }
+    return res.status(200).send(posts);
+  }
+  async getPost(req: Request, res: Response) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).send(errors);
+    }
+    try {
+      const post = await Post.findById(req.params.id)
+        .populate({
+          path: 'publishedBy',
+          model: User,
+          select: 'name'
+        })
+        .populate({
+          path: 'comments',
+          model: Comments,
+          match: { enabled: true },
+          select: 'content'
+        });
+      if (!post) {
+        return res.status(204).send({ message: 'Publicación no encontrada.' });
+      }
+      return res.status(200).send(post);
+    } catch (error) {
+      return res.status(500).send({ message: 'Ha ocurrido un error' });
+    }
+  }
+  async modifyPost(req: Request, res: Response) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).send(errors);
+    }
+    try {
+      const { title, url, content, categorys, tags } = req.body;
+      let posturl = await Post.findById(req.params.id);
+      let post = await Post.findOne({ $or: [{ url }, { title }] });
+      if (!posturl) {
+        return res.status(204).send({ message: 'Publicación no encontrada.' });
+      } else if (post) {
+        return res.status(200).send({
+          message: 'Ya existe una publicación con ese titulo o url'
+        });
+      } else if (req.userId == post.publishedBy || req.userRole === 'ADMIN') {
+        post.title = title;
+        post.url = url;
+        post.content = content;
+        post.categorys = categorys;
+        post.tags = tags;
+        post.updatedAt = Date.now();
+        await post.save();
+        return res.status(200).send({ message: 'Cambios realizados.' });
+      } else {
+        return res.status(204).send({
+          message: 'No posees permiso para modificar esta publicación.'
+        });
+      }
+    } catch (error) {
+      return res.status(500).send({ message: 'Se ha producido un error' });
+    }
+  }
+  async changePostEnabled(req: Request, res: Response) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).send(errors);
+    }
+    try {
+      let post = await Post.findById(req.params.id);
+      if (!post)
+        return res.status(204).send({ message: 'Publicación no encontrada.' });
+      if (req.userId == post.publishedBy || req.userRole === 'ADMIN') {
+        post.enabled = !post.enabled;
+        await post.save();
+        //post.deleteOne(); en caso de querer borrar de verdad
+        return res.status(200).send({
+          message: 'Se cambio la visualización de la publicacion'
+        });
+      } else {
+        return res.status(200).send({
+          message: 'No posees permiso para eliminar esta publicación'
+        });
+      }
+    } catch (error) {
+      console.log(error);
+      return res.status(400).send(error);
+    }
+  }
+  async filter(req: Request, res: Response) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).send(errors);
+    }
+    const { title, categorys, tags } = req.body;
+    let posts = await Post.find({
+      enabled: true,
+      ...(title ? { title: { $regex: title, $options: 'i' } } : {}),
+      ...(categorys ? { categorys: { $all: categorys } } : {}),
+      ...(tags ? { tags: { $all: tags } } : {})
+    })
+      .select({ title: 1, content: 1, categorys: 1 })
+      .sort({ createdAt: -1 });
+    if (!posts) {
+      return res
+        .status(400)
+        .send({ message: 'No se encontraron publicaciones' });
+    }
+    return res.status(200).send(posts);
+  }
 }
--- src/post/post.routes.ts
+++ src/post/post.routes.ts
@@ -7,54 +7,56 @@ const postController = new PostController();
 const postRouter: Router = Router();
 
 postRouter.get('/publications', postController.getPosts);
-postRouter.get('/publications/filter', postController.filter);
 postRouter.get(
-	'/publications/:id',
-	[param(['id', 'se necesita la url de la publicacion'])],
-	postController.getPost
+  '/publications/:id',
+  [param(['id', 'se necesita la url de la publicacion'])],
+  postController.getPost
 );
 postRouter.post(
-	'/publish',
-	[
-		check('title').exists().withMessage('Titulo necesario').isString(),
-		check('url').exists().withMessage('URL necesario').isURL(),
-		check('content').exists().withMessage('Contenido necesario').isString(),
-		check('categorys')
-			.exists()
-			.withMessage('Se necesita al menos una categoria')
-			.isArray(),
-		check('tags')
-			.exists()
-			.withMessage('Se necesita al menos un tag')
-			.isArray()
-	],
-	AuthMiddleware.tokenValidation,
-	postController.savePost
+  '/publications/filter',
+  [
+    check('title').optional().isString().notEmpty(),
+    check('categorys').optional().isArray().notEmpty(),
+    check('tags').optional().isArray().notEmpty()
+  ],
+  postController.filter
+);
+postRouter.post(
+  '/publish',
+  [
+    check('title').exists().withMessage('Titulo necesario').isString(),
+    check('url').exists().withMessage('URL necesario').isURL(),
+    check('content').exists().withMessage('Contenido necesario').isString(),
+    check('categorys')
+      .exists()
+      .withMessage('Se necesita al menos una categoria')
+      .isArray(),
+    check('tags').exists().withMessage('Se necesita al menos un tag').isArray()
+  ],
+  AuthMiddleware.tokenValidation,
+  postController.savePost
 );
 postRouter.put(
-	'/publications/:id',
-	[
-		param(['id', 'se necesita la url de la publicacion']),
-		check('title').exists().withMessage('Titulo necesario').isString(),
-		check('url').exists().withMessage('URL necesario').isURL(),
-		check('content').exists().withMessage('Contenido necesario').isString(),
-		check('categorys')
-			.exists()
-			.withMessage('Se necesita al menos una categoria')
-			.isArray(),
-		check('tags')
-			.exists()
-			.withMessage('Se necesita al menos un tag')
-			.isArray()
-	],
-	AuthMiddleware.tokenValidation,
-	postController.modifyPost
+  '/publications/:id',
+  [
+    param(['id', 'se necesita la url de la publicacion']),
+    check('title').exists().withMessage('Titulo necesario').isString(),
+    check('url').exists().withMessage('URL necesario').isURL(),
+    check('content').exists().withMessage('Contenido necesario').isString(),
+    check('categorys')
+      .exists()
+      .withMessage('Se necesita al menos una categoria')
+      .isArray(),
+    check('tags').exists().withMessage('Se necesita al menos un tag').isArray()
+  ],
+  AuthMiddleware.tokenValidation,
+  postController.modifyPost
 );
 postRouter.patch(
-	'/publications/:id',
-	[param(['id', 'se necesita la url de la publicacion'])],
-	AuthMiddleware.tokenValidation,
-	postController.changePostEnabled
+  '/publications/:id',
+  [param(['id', 'se necesita la url de la publicacion'])],
+  AuthMiddleware.tokenValidation,
+  postController.changePostEnabled
 );
 
 export default postRouter;
