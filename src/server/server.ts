f4ec9875f82d592bfaf429374b534c8a7f60a07c
Jordan <jordan.porras1@gmail.com>
2020-09-25 23:01:45 -0300

    add cors

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import userRouter from '../user/user.routes';
 import authRouter from '../auth/auth.routes';
 import postRouter from '../post/post.routes';
@@ -6,36 +7,37 @@ import reportRouter from '../reports/report.routes';
 import commentRouter from '../comments/comment.routes';
 
 export default class Server {
-	private app: express.Application;
-	private port: number;
+  private app: express.Application;
+  private port: number;
 
-	private constructor(port: number) {
-		this.app = express();
-		this.port = port;
-	}
+  private constructor(port: number) {
+    this.app = express();
+    this.port = port;
+  }
 
-	static init(port: number) {
-		return new Server(port);
-	}
+  static init(port: number) {
+    return new Server(port);
+  }
 
-	start() {
-		this.configuration();
-		this.routes();
-		this.app.listen(this.port, () => {
-			console.log('Server on port: ', this.port);
-		});
-	}
+  start() {
+    this.configuration();
+    this.routes();
+    this.app.listen(this.port, () => {
+      console.log('Server on port: ', this.port);
+    });
+  }
 
-	private configuration() {
-		this.app.use(express.urlencoded({ extended: false }));
-		this.app.use(express.json());
-	}
+  private configuration() {
+    this.app.use(express.urlencoded({ extended: false }));
+    this.app.use(express.json());
+    this.app.use(cors());
+  }
 
-	private routes() {
-		this.app.use(userRouter);
-		this.app.use(authRouter);
-		this.app.use(postRouter);
-		this.app.use(commentRouter);
-		this.app.use(reportRouter);
-	}
+  private routes() {
+    this.app.use(userRouter);
+    this.app.use(authRouter);
+    this.app.use(postRouter);
+    this.app.use(commentRouter);
+    this.app.use(reportRouter);
+  }
 }
