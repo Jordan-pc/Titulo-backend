3273d42f9e17a7a95d0475c46e4378ce920f8fe4
Jordan <jordan.porras1@gmail.com>
2020-09-30 20:44:32 -0300

    Se modifica endpoint que entrega el access token

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,33 +4,29 @@ import User, { IUser } from '../user/user.model';
 import jwt from 'jsonwebtoken';
 
 export default class AuthController {
-	async logIn(req: Request, res: Response) {
-		const errors = validationResult(req);
-		if (!errors.isEmpty()) {
-			return res.status(400).send(errors);
-		}
-		const { email, password } = req.body;
-		let user: IUser | null = await User.findOne({
-			$and: [{ email }, { enabled: true }]
-		});
-		if (!user)
-			return res
-				.status(400)
-				.send({ message: 'Email o contraseña incorrecta' });
-		if (!(await user.validatePassword(password)))
-			return res
-				.status(400)
-				.send({ message: 'Email o contraseña incorrecta' });
-		const token: string = jwt.sign(
-			{
-				_id: user._id,
-				role: user.role
-			},
-			'token-dev',
-			{
-				expiresIn: '24H'
-			}
-		);
-		res.header('Authorization', token).send(user);
-	}
+  async logIn(req: Request, res: Response) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).send(errors);
+    }
+    const { email, password } = req.body;
+    let user: IUser | null = await User.findOne({
+      $and: [{ email }, { enabled: true }]
+    });
+    if (!user)
+      return res.status(400).send({ message: 'Email o contraseña incorrecta' });
+    if (!(await user.validatePassword(password)))
+      return res.status(400).send({ message: 'Email o contraseña incorrecta' });
+    const token: string = jwt.sign(
+      {
+        _id: user._id,
+        role: user.role
+      },
+      'token-dev',
+      {
+        expiresIn: '24H'
+      }
+    );
+    return res.status(200).send({ accessToken: token });
+  }
 }
