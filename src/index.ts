4886e5b730e582b186c33d28d0410db8adb297bf
Jordan <jordan.porras1@gmail.com>
2020-11-01 19:25:02 -0300

    connect mongodb atlas

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import Server from './server/server';
 import DBconnect from './database/database.connect';
 
-const url: string = 'mongodb://localhost:27017/PCollabRAE-DB';
+const url: string =
+  'mongodb+srv://JordanAdmin:kH7bknkbETXGj6lU@titulo.a9ame.mongodb.net/tituloPortal?retryWrites=true&w=majority';
 const database = DBconnect.init(url);
 const server = Server.init(Number(process.env.PORT || 3000));
 
